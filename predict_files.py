4f5910e1b51d4f4d8a27f6cdd02000b884f675d8
Joost Bottenbley <38734574+Joost001@users.noreply.github.com>
2022-12-13 15:12:01 -0500

    made control section @ begining of program

--- predict_files.py
+++ predict_files.py
@@ -3,6 +3,11 @@ from tqdm import tqdm
 import helpers.asr as asr
 import re
 
+# program controls
+asr_mapping_file = '/Volumes/Boston/Scratch/dli_del/wav_split_gold/gold_asr_mapping.csv'
+pred_output = './output/results.csv'
+
+
 def post_processing_transcription(string):
     string = string.strip('.«,').replace('=', '').replace(' ', '').replace('Ø', ' ')
     string = re.sub('\(.+?\)', '', string)
@@ -16,9 +21,10 @@ def post_processing_recognized(string):
     string = string if string else '-'
     return string
 
+
 # read asr golden labels
 print("Reading file...")
-df_gl = pd.read_csv('/Volumes/Boston/Scratch/dli_del/wav_split_gold/gold_asr_mapping.csv')
+df_gl = pd.read_csv(asr_mapping_file)
 
 # read in each of the converted files
 lof = df_gl['full_path_outfile'].to_list()
@@ -41,4 +47,4 @@ df_gl['transcription_clean'] = df_gl['transcription'].apply(post_processing_tran
 df_gl['recognized_clean'] = df_gl['recognized'].apply(post_processing_recognized)
                                             
 # save results to csv file
-df_gl.to_csv('./output/results.csv')
+df_gl.to_csv(pred_output)
